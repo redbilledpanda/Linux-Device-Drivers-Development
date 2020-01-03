f91616e340e56a5595d66232eca25fc1c62b22cd
Aijaz Baig <Aijaz.Baig@protonmail.com>
2020-01-03 11:41:55 +0530

    Showing time in ms for waitqueue sample program

--- Chapter03/waitqueue.c
+++ Chapter03/waitqueue.c
@@ -3,7 +3,10 @@
 #include <linux/sched.h>
 #include <linux/time.h>
 #include <linux/delay.h>
-#include<linux/workqueue.h>
+#include <linux/workqueue.h>
+
+// -- AIJAZ --
+#include <linux/jiffies.h>
 
 static DECLARE_WAIT_QUEUE_HEAD(my_wq);
 static int condition = 0;
@@ -13,7 +16,7 @@ static struct work_struct wrk;
 
 static void work_handler(struct work_struct *work)
 { 
-    pr_info("Waitqueue module handler %s\n", __FUNCTION__);
+    pr_info("Waitqueue module handler %s, ms cnt is %u\n", __FUNCTION__, jiffies_to_msecs(jiffies));
     msleep(3000);
     pr_info("Wake up the sleeping module\n");
     condition = 1;
@@ -22,7 +25,7 @@ static void work_handler(struct work_struct *work)
 
 static int __init my_init(void)
 {
-    pr_info("Wait queue example\n");
+    pr_info("Wait queue example, ms count is %u\n", jiffies_to_msecs(jiffies));
 
     INIT_WORK(&wrk, work_handler);
     schedule_work(&wrk);
@@ -30,7 +33,7 @@ static int __init my_init(void)
     pr_info("Going to sleep %s\n", __FUNCTION__);
     wait_event_interruptible(my_wq, condition != 0);
 
-    pr_info("woken up by the work job\n");
+    pr_info("woken up by the work job at %ums\n", jiffies_to_msecs(jiffies));
     return 0;
 }
 
